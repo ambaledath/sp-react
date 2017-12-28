68fa162e87c719fb54064f840bc215df5a17c9e7
Prasad Ambaledath <psd.amb@gmail.com>
2017-12-28 20:18:00 +0530

    contact section modified

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -18,6 +18,17 @@ class Contact extends Component {
     return (
         <div className="contact">
             <h1 className="container">Contact</h1>
+            <div className="container">
+                <form>
+                    <label htmlFor="email">Email</label>
+                    <input required type="email" id="email" className="form-control" placeholder="your email" />
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" className="form-control" placeholder="your name" />
+                    <label htmlFor="message">Message</label>
+                    <textarea id="message" className="form-control" placeholder="your message"></textarea>
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Send</button>
+                </form>
+            </div>
             <MyMapComponent
               isMarkerShown
               googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
@@ -25,26 +36,6 @@ class Contact extends Component {
               containerElement={<div style={{ height: `400px` }} />}
               mapElement={<div style={{ height: `100%` }} />}
             />
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-6">
-                        <form>
-                            <label htmlFor="email">Email</label>
-                            <input required type="email" id="email" className="form-control" placeholder="enter your email" />
-                            <label htmlFor="name">Name</label>
-                            <input type="text" id="name" className="form-control" placeholder="enter your name" />
-                            <label htmlFor="message">Message</label>
-                            <textarea id="message" className="form-control" placeholder="enter your message"></textarea> 
-                            <button className="btn btn-lg btn-primary btn-block" type="submit">Send</button>
-                        </form>
-                    </div>
-                    <div className="col-lg-6">
-                        <address className="text-muted">
-                            <h3>Lorem Ipsum</h3>
-                        </address>
-                    </div>
-                </div>
-            </div>
         </div>
     );
   }
